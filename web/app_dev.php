278d9c0d80e36a209f20a9b5f52fce83cb8c8656
Sukach <sukach.v.v@gmail.com>
2016-01-27 18:56:17 +0200

    fix

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,7 +13,7 @@ use Symfony\Component\Debug\Debug;
 var_dump('hello');
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1', '192.168.10.1']) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1', '91.221.166.69']) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
